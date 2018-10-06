61fc94b791472defed7046bd17edf016fb5e1864
Adesina Tyler <adesinat@csedge.org>
2018-10-06 13:18:34 -0700

    I added 3 number variables, 3 string variables, two boolean variables, and one undefined variable

--- data.js
+++ data.js
@@ -1,5 +1,20 @@
 // Example
-let message = "Hello World!";
+let message = "360 Ogga Booga";
 console.log(message);
-
+//Numbers
+var 1;
+let 0.1 = "Atom";
+var 999 = "plusOne";
+var 66 = "Suppressed";
+//Boolean
+var "360oggaBooga";
+let "360oggaBooga" = true;
+var "waterWet";
+let "waterWet" = true;
+//Strings
+var "4thJuly" = "spangled";
+var "SmashUlt" = "bandanaDee";
+var "BrOtHeR";
+//Undefined
+var "NaN" = undefined;
 // Type your code below!
